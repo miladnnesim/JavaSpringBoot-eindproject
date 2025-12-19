9e5f373d1400addc5d6cc9757bfb99a63481112a
miladnnesim <milad.nesim@student.ehb.be>
2025-12-19 17:38:12 +0100

    maven init

--- README.md
+++
@@ -1,4 +0,0 @@
-# JavaSpringBoot-eindproject
-
-# Sources
-https://chatgpt.com/share/6945733f-d248-8011-bba7-9a2996cd56e4
--- src/test/java/ehb/demo/DemoApplicationTests.java
+++ src/test/java/ehb/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package ehb.eindrpoject;
+package ehb.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EIndrpojectApplicationTests {
+class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
