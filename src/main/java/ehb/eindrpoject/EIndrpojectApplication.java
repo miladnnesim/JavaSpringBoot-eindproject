c16eb1c31778b73aea321a0d4cf0defe56cc2ee8
miladnnesim <milad.nesim@student.ehb.be>
2025-12-19 17:27:46 +0100

    init project

---
+++ src/main/java/ehb/eindrpoject/EIndrpojectApplication.java
@@ -0,0 +1,13 @@
+package ehb.eindrpoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EIndrpojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EIndrpojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ehb/eindrpoject/EIndrpojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ehb.eindrpoject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EIndrpojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
