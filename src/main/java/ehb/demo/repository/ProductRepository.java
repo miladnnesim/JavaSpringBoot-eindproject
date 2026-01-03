a3bcea1c3bf32ecf8ecf3ea20762860517bcd305
miladnnesim <milad.nesim@student.ehb.be>
2026-01-03 13:30:38 +0100

    product repo

--- src/main/java/ehb/demo/repository/ProductRepository.java
+++ src/main/java/ehb/demo/repository/ProductRepository.java
@@ -1,2 +1,14 @@
-package ehb.demo.repository;public class ProductRepository {
+package ehb.demo.repository;
+
+import ehb.demo.model.Product;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    // Voeg 'Sort' toe aan de parameters van je bestaande methodes
+    List<Product> findByCategorieAndNaamContainingIgnoreCase(String categorie, String zoekterm, Sort sort);
+    List<Product> findByCategorie(String categorie, Sort sort);
+    List<Product> findByNaamContainingIgnoreCase(String zoekterm, Sort sort);
+    List<Product> findAll(Sort sort);
 }
